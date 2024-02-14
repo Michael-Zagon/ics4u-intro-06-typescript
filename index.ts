3d91650fdfe506ac6dbffe41ade65eeca7e18807
Michael <michael.zagon@mths.ca>
2024-02-14 22:23:18 +0000

    helloworld with comments for intro-06

---
+++ index.ts
@@ -0,0 +1,12 @@
+/**
+ * * The HelloWorld program implements an application that
+ * * simply displays "Hello World!" to the standard output.
+ * *
+ * * By:      Michael Zagon
+ * * Version: 1.0
+ * * Since:   2024-02-14
+ * */
+
+console.log("Hello, World!")
+
+console.log("\nDone")
